df7caf825e8244ee876afdc29cd66fa2956e0b56
PrevailExcel <prevailexcellent@gmail.com>
2025-02-07 17:04:18 +0100

    Remove unused components: Loader, Splash, Head, Notifications, DepositTransactions, Fund, Transaction, and Menu

--- src/components/Loader.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-</script>
-
-<template>
-    <div class="backdrop"></div>
-    <div class="bouncing-loader">
-        <img src="/loader.gif" width="90" class="rounded-3 loader" />
-    </div>
-</template>
