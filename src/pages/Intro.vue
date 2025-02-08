6930549eb2e71fcc9fe1fb2de530b5e8bfb44874
PrevailExcel <prevailexcellent@gmail.com>
2025-02-08 08:55:21 +0100

    Refactor Home and Intro pages; clean up imports and improve formatting in HTML and CSS

--- src/pages/Intro.vue
+++ src/pages/Intro.vue
@@ -12,7 +12,7 @@ const items = [1, 2, 3, 4, 55, 5, 5, 5, 5]; // Replace with actual data if neces
             <div class="overlay"></div>
             <div class="app-bar">
                 <button class="btn text-white fs-4" @click="$router.go(-1)">
-                    <img src="/icons/back1.png" width="24"> 
+                    <img src="/icons/back1.png" width="24">
                 </button>
             </div>
             <div>
@@ -25,16 +25,16 @@ const items = [1, 2, 3, 4, 55, 5, 5, 5, 5]; // Replace with actual data if neces
             <h2 class="f716 mt-5">Storm Warning</h2>
             <h4 class="f616">Ejimadu Prevail C.</h4>
             <div class="text-start px-md-5 mt-md-5">
-            <h3 class="f716 mt-3">About the Book</h3>
-            <p class="intro">
-                In this captivating and thought-provoking novel, readers are 
-transported into a world where the boundaries of reality and 
-imagination blur. With richly drawn characters and a plot that 
-twists and turns, this book explores themes of love, loss, and 
-the complexity of human nature. As the story unfolds, secrets 
-are revealed, relationships are tested, and the characters are 
-forced to confront their deepest fears and desires.
-            </p>
+                <h3 class="f716 mt-3">About the Book</h3>
+                <p class="intro">
+                    In this captivating and thought-provoking novel, readers are
+                    transported into a world where the boundaries of reality and
+                    imagination blur. With richly drawn characters and a plot that
+                    twists and turns, this book explores themes of love, loss, and
+                    the complexity of human nature. As the story unfolds, secrets
+                    are revealed, relationships are tested, and the characters are
+                    forced to confront their deepest fears and desires.
+                </p>
             </div>
             <button class="btn btn-brown mt-2">Start Reading</button>
         </div>
